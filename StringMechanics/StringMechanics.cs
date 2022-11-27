28aad980d0ec026ced7c629fc202d92b1abc3c3a
Anton Lugom <mogul086@mail.ru>
2022-11-27 20:25:57 +0300

    initial

---
+++ StringMechanics/StringMechanics.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq.Expressions;
+
+namespace StringMechanics
+{
+    public class StringMechanics
+    {
+        /// <summary>
+        /// Разделяет строку на слова, разделителем считается символ пробела
+        /// </summary>
+        /// <param name="text">Строка которую необходимо разделить</param>
+        /// <returns>Массив слов разделенной строки</returns>
+        public static string[] SplitText(string text)
+        {
+            int countWords = 1;
+            char[] arrayOfLetters = new char[text.Length];
+            int charIndex = 0;
+            foreach (var letter in text)
+            {
+                arrayOfLetters[charIndex] = letter;
+                charIndex++;
+                if (letter == ' ')
+                {
+                    countWords++;
+                }
+            }
+
+            string[] dividedText = new string[countWords];
+
+            int wordNumberIndex = 0;   
+            for (charIndex = 0; charIndex < arrayOfLetters.Length; charIndex++)
+            {
+                if (arrayOfLetters[charIndex] != ' ')
+                {
+                    dividedText[wordNumberIndex] += arrayOfLetters[charIndex];
+                }
+                else
+                {
+                    wordNumberIndex++;
+                }
+            }
+           
+
+            return dividedText;
+        }
+
+        /// <summary>
+        /// Вывод в консоль массива строк каждый элемент на новой строке
+        /// </summary>
+        /// <param name="arrayToWrite">Массив для вывода в консоль</param>
+        public static void WriteText(string[] arrayToWrite)
+        {
+            for (int wordsIndex = 0; wordsIndex < arrayToWrite.Length; wordsIndex++)
+            {
+                Console.WriteLine(arrayToWrite[wordsIndex]);
+            }
+        }
+
+        /// <summary>
+        /// Возвращает строку с обратной последовательностью слов
+        /// </summary>
+        /// <param name="text">Текст для разворота слов</param>
+        /// <returns>Результат смены последовательности слов, при неуданом преобразовании
+        /// возращается изначальная строка</returns>
+        public static string ReverseWords(string text)
+        {
+            string? result = null;
+            string[] initialText = SplitText(text);
+
+            for (int wordIndex = initialText.Length-1; wordIndex>=0; wordIndex--)
+            {
+                result += initialText[wordIndex] + ' ';
+            }
+
+
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
---
+++ StringMetods/Program.cs
@@ -0,0 +1,99 @@
+﻿using static StringMechanics.StringMechanics;
+
+namespace StringMetods
+{
+    /// <summary>
+    /// Практическое задание 5. Методы
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка старта
+        /// </summary>
+        static void Main()
+        {
+            #region Задание 1
+
+            Console.WriteLine("Практическое задание 5. Методы");
+            Console.WriteLine("Задание 1. Разделение строки на слова");
+            Console.WriteLine("Можно воспользоваться преденными примерами для" +
+                                " разделения или ввести свою строку");
+            Console.WriteLine("Введите цифру выбранного варианта "+
+                              "или цифру '3' для своего ввода ");
+
+            string textExample1 = "Извлекает подстроку из данного экземпляра";
+            string textExample2 = "Пользователь вводит в консольном приложении длинное предложение";
+
+            Console.WriteLine($"1. {textExample1}");
+            Console.WriteLine($"2. {textExample2}");
+
+            int correctInput;
+
+            do
+            {
+                Console.WriteLine("Введите целое число от 1 до 3: ");
+                string userInput = Console.ReadLine().ToString();
+                int.TryParse(userInput, out correctInput);
+
+            } while (!(0 < correctInput  && correctInput < 4));
+
+            string[] dividedText;
+            string userTextChoice;
+            switch (correctInput)
+            {
+                case 1:
+                    userTextChoice = textExample1;
+                    dividedText = SplitText(userTextChoice);
+                    WriteText(dividedText);
+                    break;
+                case 2:
+                    userTextChoice = textExample2;
+                    dividedText = SplitText(userTextChoice);
+                    WriteText(dividedText);
+                    break;
+                default:
+                    userTextChoice = Console.ReadLine();
+                    
+                    if ( !string.IsNullOrEmpty(userTextChoice))
+                    {
+                        dividedText = SplitText (userTextChoice);
+                        WriteText(dividedText);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели пустую строку, нечего делить");
+                    }
+                    break;
+            }
+
+            Console.ReadKey(true);
+            Console.Clear();
+
+            #endregion
+
+            #region Задание 2
+            Console.WriteLine("Практическое задание 5. Методы");
+            Console.WriteLine("Задание 2. Перестановка слов в предложении");
+            Console.WriteLine("Будет использована строка из первого задания");
+
+            Console.WriteLine("\nИзначальная строка:");
+            Console.WriteLine(userTextChoice);
+
+            if (!string.IsNullOrEmpty(userTextChoice))
+            {
+                string reverseText = ReverseWords(userTextChoice);
+                Console.WriteLine("\nРазвернутая строка:");
+                Console.WriteLine(reverseText);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели пустую строку, нечего делить и разворачивать");
+            }
+
+            Console.ReadKey(true);
+
+            #endregion
+
+        }
+    }
+}
